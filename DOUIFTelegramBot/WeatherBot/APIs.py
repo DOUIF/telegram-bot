ab7f75e3ce5499355522830b6de7edd39e60d58b
DOUIF <a15975345678@gmail.com>
2022-08-25 04:58:43 +0800

    refactor: 刪除 weather_bot 的API.json檔案

--- DOUIFTelegramBot/WeatherBot/APIs.py
+++ DOUIFTelegramBot/WeatherBot/APIs.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass, field
-import json
+from dataclasses import dataclass
 from pathlib import Path
 
 CURRENT_PATH = Path(__file__).parent
@@ -8,13 +7,10 @@ API_JSON_FILE = f"{CURRENT_PATH}\\APIs.json"
 
 @dataclass
 class APIs:
-    __HOST_URL: str = field(init=False)
-    __API_URL: dict = field(init=False)
-
-    def __post_init__(self):
-        api_json = json.load(open(API_JSON_FILE, "r"))
-        self.__HOST_URL = api_json["HOST_URL"]
-        self.__API_URL = api_json["API_URL"]
+    __HOST_URL: str = "https://opendata.cwb.gov.tw/api"
+    __API_URL: dict = {
+        "36HOUR_FORECAST": "v1/rest/datastore/F-C0032-001",
+    }
 
     def get_url(self, api_name: str) -> str:
         if api_name in self.__API_URL:
