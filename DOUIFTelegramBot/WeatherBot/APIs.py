d2d3d4754b9993c69969ee7bc71e2deb2b3f288f
DOUIF <a15975345678@gmail.com>
2022-08-25 05:08:50 +0800

    fix: APIs.py's dataclass bug

--- DOUIFTelegramBot/WeatherBot/APIs.py
+++ DOUIFTelegramBot/WeatherBot/APIs.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from pathlib import Path
 
 CURRENT_PATH = Path(__file__).parent
@@ -8,9 +8,12 @@ API_JSON_FILE = f"{CURRENT_PATH}\\APIs.json"
 @dataclass
 class APIs:
     __HOST_URL: str = "https://opendata.cwb.gov.tw/api"
-    __API_URL: dict = {
-        "36HOUR_FORECAST": "v1/rest/datastore/F-C0032-001",
-    }
+    __API_URL: dict = field(default_factory=dict)
+
+    def __post_init__(self):
+        self.__API_URL = {
+            "36HOUR_FORECAST": "v1/rest/datastore/F-C0032-001",
+        }
 
     def get_url(self, api_name: str) -> str:
         if api_name in self.__API_URL:
