3461d59ffff10ad5054a7b59da4e350e49d0c847
DOUIF <a15975345678@gmail.com>
2022-08-25 15:47:21 +0800

    refactor: delete credential json

--- douiftelegrambot/weatherbot/data_crawler.py
+++ douiftelegrambot/weatherbot/data_crawler.py
@@ -5,13 +5,10 @@ import requests
 from .apis import APIs as API_URL
 from pathlib import Path
 
-CURRENT_PATH = Path(__file__).parents[0]
-print(CURRENT_PATH)
-CREDENTIAL = json.load(open(f"{CURRENT_PATH}\\credential.json", "r"))["credential"]
-
 
 @dataclass
 class DataCrawler:
+    __api_token: str = "CWB-79DFE6D9-0EFB-4F48-947B-01982F16C096"
     __api_url: API_URL = API_URL()
 
     def get_36hour_forecast(self, **kwargs) -> dict:
@@ -20,7 +17,7 @@ class DataCrawler:
         self.__get_data_from_url(url, kwargs)
 
     def __get_data_from_url(self, url: str, param: dict) -> dict:
-        param["Authorization"] = CREDENTIAL
+        param["Authorization"] = self.__api_token
         data = requests.get(url=url, params=param)
         if data.status_code == 200:
             print(json.loads(data.text))
